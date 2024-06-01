6907d36db3c8ec517f0ee9a4cf0b44b5728d3652
Maverick Wong <mvrck.wong@gmail.com>
2024-06-01 17:32:37 +0800

    update

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,5 +1,56 @@
 from config import DATA_DIR, PRJ_DIR, SRC_DIR
+from dotenv import load_dotenv
+
+from os import getenv
+from pathlib import Path
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_csv_to_db(csv_file:str, engine:create_engine):
+    
+    # Extract table name from the CSV file name
+    table_name = csv_file.stem.capitalize()
+    df = pd.read_csv(csv_file)
+    
+    # Load DataFrame into the database
+    df.to_sql(
+        table_name, engine, if_exists='replace', index=False
+    )
+
+def main(csv_dir=Path | str):
+    
+    # Preprocess the args
+    if isinstance(csv_dir, str):
+        csv_dir = Path(csv_dir)
+    
+    # Specify the dotenv path
+    env_file = SRC_DIR / ".env"
+    if not env_file.exists():
+        raise "Environment file not found!"
+    else:
+        # Database Credentials
+        db_host = getenv('DB_HOST')
+        db_user = getenv('DB_USER')
+        db_pw = getenv('DB_PW')
+        db_name = getenv('DB_NAME')
+    
+    # Load environment variables from .env file
+    load_dotenv(env_file)
+    
+    # Database connection details
+    db_uri = f'postgresql://{db_user}:{db_pw}@{db_host}/{db_name}?sslmode=require'
+    db_engine = create_engine(db_uri)
+    
+    # Iterate through all CSV files in the directory and load them into the database
+    for csv_file in csv_dir.glob('*.csv'):
+        load_csv_to_db(csv_file, db_engine)
+        
+    return True
+
 
 # Running the main function
 if __name__ == "__main__":
-    None
+    if main(DATA_DIR):
+        print("Main function finished successfully.")
