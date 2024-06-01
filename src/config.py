4f83b3c5b8831e6dfb8d270a2ed5792ab76115d5
Maverick Wong <mvrck.wong@gmail.com>
2024-06-01 15:12:02 +0800

    update

---
+++ src/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+SRC_DIR = Path(__file__).parent
+PRJ_DIR = SRC_DIR.parent
+DATA_DIR = PRJ_DIR / "data"
+
+# Running the main function
+if __name__ == "__main__":
+    None
---
+++ src/ingestion.py
@@ -0,0 +1,5 @@
+from config import DATA_DIR, PRJ_DIR, SRC_DIR
+
+# Running the main function
+if __name__ == "__main__":
+    None
